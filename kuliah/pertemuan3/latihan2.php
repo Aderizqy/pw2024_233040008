5183c4c46dd3c83b375fbef716bb49d8c99d9dc5
Aderizqy <aderizqy99@gmail.com>
2024-02-23 20:29:17 +0700

    first commit

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,6 @@
+<?php
+$nama = "Ade Rizqy";
+$umur = 20;
+$unpas = "Universitas Pasundan";
+echo "Halo, nama saya $nama. Umur saya $umur tahun, saya kuliah di $unpas.";
+echo "<br>";
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,12 @@
+<?php
+$npm_ade = 37; // Ganti dengan dua digit terakhir NPM Anda
+echo "Aku adalah angka <strong> $npm_ade </strong> <br>";
+$npm_ade *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <strong> $npm_ade </strong> <br>";
+$npm_ade /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <strong> $npm_ade </strong> <br>";
+$npm_ade += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <strong> $npm_ade </strong> <br>";
+$npm_ade -= 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <strong> $npm_ade </strong> <br>";
+?>
